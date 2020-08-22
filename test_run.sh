ff2dbd40adf37fba4e1dcfa4f3000f83fff7a2b8
eshamster <hamgoostar@gmail.com>
2020-08-23 00:12:25 +0900

    Remove temporal solution for emacs 26.2-
    The latest of cl-base has been updated, then now Emacs 26.3 can be
    installed by apk command. So, temporal solution for 26.2 about SSL
    is no longer required.

--- test_run.sh
+++ test_run.sh
@@ -4,8 +4,13 @@ set -eu
 
 name=cl-devel2
 
+no_cache_opt=
+if [ $? -ge 1 ]; then
+  no_cache_opt="--no-cache"
+fi
+
 docker rmi $(docker images | awk '/^<none>/ { print $3 }') || echo "ignore rmi error"
 docker rm `docker ps -a -q` || echo "ignore rm error"
 
-docker build -t ${name}:latest .
+docker build ${no_cache_opt} -t ${name}:latest .
 docker run --name ${name} -v ~/work/lisp:/root/work -v "$(pwd)/sample-custom-el":/root/.emacs.d/site-lisp/custom -it ${name}:latest /bin/ash
